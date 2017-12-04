11509514f6ca25e67a47402c13ad31a3cfdf0a80
candyforfun <liaojianyi95319@gmail.com>
2017-12-04 17:44:29 +0800

    1

---
+++ src/main/java/com/sug/core/util/BigDecimalUtils.java
@@ -0,0 +1,19 @@
+package com.sug.core.util;
+
+import java.math.BigDecimal;
+
+public class BigDecimalUtils {
+    public static BigDecimal add(BigDecimal value1,BigDecimal value2){
+        return value1.add(value2).setScale(2,BigDecimal.ROUND_UP);
+    }
+
+    public static BigDecimal subtract(BigDecimal value1,BigDecimal value2){
+        return value1.subtract(value2).setScale(2,BigDecimal.ROUND_UP);
+    }
+
+    public static BigDecimal multiply(BigDecimal value1,BigDecimal value2){
+        return value1.multiply(value2).setScale(2,BigDecimal.ROUND_UP);
+    }
+
+
+}
