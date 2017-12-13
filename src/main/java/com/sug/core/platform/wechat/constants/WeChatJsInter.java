8c703415923c30b7cf57346352881e41d6c1dc44
candyforfun <liaojianyi95319@gmail.com>
2017-12-14 00:40:34 +0800

    wechat config interface

--- src/main/java/com/sug/core/platform/wechat/constants/WeChatJsInter.java
+++ src/main/java/com/sug/core/platform/wechat/constants/WeChatJsInter.java
@@ -2,4 +2,6 @@ package com.sug.core.platform.wechat.constants;
 
 public class WeChatJsInter {
     public static final String JS_PAY = "chooseWXPay";
+    public static final String TIMELINE_SHARE = "onMenuShareTimeline";
+    public static final String APPMESSAGE_SHARE = "onMenuShareAppMessage";
 }
