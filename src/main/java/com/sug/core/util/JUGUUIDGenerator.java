32e1e6ebddb5b68d94ea51a5c900529ebadd88c0
candyforfun <liaojianyi95319@gmail.com>
2018-03-26 19:35:23 +0800

    uuid

--- src/main/java/com/sug/core/util/JUGUUIDGenerator.java
+++ src/main/java/com/sug/core/util/JUGUUIDGenerator.java
@@ -3,6 +3,7 @@ package com.sug.core.util;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedGenerator;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class JUGUUIDGenerator {
@@ -15,5 +16,11 @@ public class JUGUUIDGenerator {
 
     public static String generate() {
         return SingletonTimeBasedGenerator.UUID_GENERATOR.generate().toString().replaceAll("-","");
+    }
+
+    public static String generateShort(Integer digit){
+        int first = new Random(10).nextInt(8) + 1;
+        String result = generate();
+        return first + result.substring(0,digit).toUpperCase();
     }
 }
